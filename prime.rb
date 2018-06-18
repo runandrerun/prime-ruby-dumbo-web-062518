97b1aa6dfc836b99a2670aabe0aff42bfcdaa8bb
Andre Santiago <the.asantiagojr@gmail.com>
2018-06-18 20:35:41 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,16 +1,5 @@
 # Add  code here!
 
 def prime?(num)
-  array = [] << num
-  array.each do |num|
-  i = 2
-  while i < num
-    if num % i == 0
-      return false
-    else
-      return true
-    end
-    i += 1
-  end  
-end
+  
 end
