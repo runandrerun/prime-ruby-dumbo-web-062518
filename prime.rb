d38e22598c9290e93da02402bb25211a8530ce99
Andre Santiago <the.asantiagojr@gmail.com>
2018-06-18 20:27:51 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,16 @@
 # Add  code here!
 
 def prime?(num)
-  array = [] << (1..num)
-  array.each do {|x| x % 2} 
+ def is_prime?(*nums)
+    nums.each do |num|
+    i = 2
+      while i < num
+        if num % i == 0
+          puts "#{num} is not a prime"
+        else
+          puts "#{num} is a prime"
+        end
+        i += 1
+      end
+   end
 end
